d4de2d7d12d45c8ad99f2c0153dcf6c99c2d10b8
Salvatore Alaimo <alaimos@gmail.com>
2021-05-17 22:38:09 +0200

    Simulation Module: new simulation page

--- src/resources/js/Pages/Modules/Simulations/Create.tsx
+++ src/resources/js/Pages/Modules/Simulations/Create.tsx
@@ -21,20 +21,52 @@ import { useForm } from "@inertiajs/inertia-react";
 import TagsManagerCard from "../../../Components/TagsManagerCard";
 import route from "ziggy-js";
 import classNames from "classnames";
-import BootstrapTable from "react-bootstrap-table-next";
-// import paginationFactory from "react-bootstrap-table2-paginator";
+import BootstrapTable, {
+    SizePerPageRendererOptions,
+} from "react-bootstrap-table-next";
+import paginationFactory from "react-bootstrap-table2-paginator";
+import filterFactory, { textFilter } from "react-bootstrap-table2-filter";
 
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
 import "react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min.css";
+import "react-bootstrap-table2-filter/dist/react-bootstrap-table2-filter.min.css";
 
-// @ts-ignore
-import ToolkitProvider, {
-    Search,
-    // @ts-ignore
-} from "react-bootstrap-table2-toolkit/dist/react-bootstrap-table2-toolkit.min";
+import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
 
 const { SearchBar } = Search;
+
+const pagination = paginationFactory({
+    page: 1,
+    alwaysShowAllBtns: true,
+    showTotal: true,
+    withFirstAndLast: false,
+    sizePerPageRenderer: ({
+        onSizePerPageChange,
+    }: SizePerPageRendererOptions) => (
+        <div className="dataTables_length" id="datatable-basic_length">
+            <label>
+                Show{" "}
+                {
+                    <select
+                        name="datatable-basic_length"
+                        aria-controls="datatable-basic"
+                        className="form-control form-control-sm"
+                        onChange={(e) =>
+                            onSizePerPageChange(1, +e.target.value)
+                        }
+                    >
+                        <option value="10">10</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                    </select>
+                }{" "}
+                entries.
+            </label>
+        </div>
+    ),
+});
 
 interface Organism {
     id: number;
@@ -201,7 +233,6 @@ const Create: React.FC<Props> = ({
                                     </Label>
                                     <div>
                                         <ToolkitProvider
-                                            bootstrap4
                                             keyField="id"
                                             data={nodes.data}
                                             columns={[
@@ -209,28 +240,61 @@ const Create: React.FC<Props> = ({
                                                     dataField: "accession",
                                                     text: "Accession",
                                                     sort: true,
+                                                    filter: textFilter(),
                                                 },
                                                 {
                                                     dataField: "name",
                                                     text: "Name",
                                                     sort: true,
+                                                    filter: textFilter(),
                                                 },
                                             ]}
                                             search
-                                            // pagination={paginationFactory({})}
                                         >
-                                            {(props: any) => (
-                                                <div>
-                                                    <h3>
-                                                        Input something at below
-                                                        input field:
-                                                    </h3>
-                                                    <SearchBar
-                                                        {...props.searchProps}
-                                                    />
-                                                    <hr />
+                                            {(props) => (
+                                                <div className="py-4">
+                                                    <Container fluid>
+                                                        <Row>
+                                                            <Col
+                                                                xs={12}
+                                                                sm={{
+                                                                    size: 6,
+                                                                    offset: 6,
+                                                                }}
+                                                            >
+                                                                <div
+                                                                    id="datatable-basic_filter"
+                                                                    className="dataTables_filter px-4 pb-1 float-right"
+                                                                >
+                                                                    <label>
+                                                                        Search:
+                                                                        <SearchBar
+                                                                            className="form-control-sm"
+                                                                            placeholder=""
+                                                                            {...props.searchProps}
+                                                                        />
+                                                                    </label>
+                                                                </div>
+                                                            </Col>
+                                                        </Row>
+                                                    </Container>
                                                     <BootstrapTable
                                                         {...props.baseProps}
+                                                        remote
+                                                        bootstrap4
+                                                        pagination={pagination}
+                                                        filter={filterFactory()}
+                                                        bordered={false}
+                                                        onTableChange={(
+                                                            type,
+                                                            newState
+                                                        ) => {
+                                                            console.log(
+                                                                type,
+                                                                newState
+                                                            );
+                                                            // 'filter' | 'pagination' | 'sort'
+                                                        }}
                                                     />
                                                 </div>
                                             )}
